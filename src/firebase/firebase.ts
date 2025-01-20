6647c5bc2f90879585b970f73dd72d96206ce404
Hwang-Jaeryeong <jryeong67@naver.com>
2025-01-20 17:09:12 +0900

    firebase private key

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,7 +7,7 @@ dotenv.config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // 줄바꿈 처리
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: "https://yeonpick-quokka.firebaseio.com", // Firebase Database URL
